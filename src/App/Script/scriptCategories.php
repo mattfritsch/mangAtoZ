850b350a783b4fd798496991165b47a2e08d831f
adriendst <70936701+adriendst@users.noreply.github.com>
2022-12-01 14:30:13 +0100

    les données des mangas sont importées dans la db

---
+++ src/App/Script/scriptCategories.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once dirname(__DIR__) . '/../../vendor/autoload.php';
+
+
+use App\Entity\Categ;
+use Framework\Doctrine\EntityManager;
+
+$categories = [];
+function getCategoriesDesc(){
+    global $categories;
+
+    for($i=0; $i<200; $i = $i +20) {
+        $json = file_get_contents('https://kitsu.io/api/edge/categories?page[limit]=20&page[offset]=' . $i);
+        $arr = json_decode($json, true);
+        foreach($arr['data'] as $row){
+            array_push($categories, [$row['id'], $row['attributes']['title'], $row['attributes']['description']]);
+        }
+    }
+    $json = file_get_contents('https://kitsu.io/api/edge/categories?page[limit]=18&page[offset]=200');
+    $arr = json_decode($json, true);
+    foreach($arr['data'] as $row){
+        array_push($categories, [$row['id'], $row['attributes']['title'], $row['attributes']['description']]);
+    }
+    var_dump($categories);
+
+    insertCategIntoDB();
+}
+
+
+function insertCategIntoDB()
+{
+    global $categories;
+    $em = EntityManager::getInstance();
+    for ($i = 0; $i < count($categories); $i++) {
+        $categ = new Categ();
+
+        $categ->setCategId($categories[$i][0]);
+        $categ->setCategName($categories[$i][1]);
+        $categ->setCategDesc($categories[$i][2]);
+
+
+        $em->persist($categ);
+        $em->flush();
+
+
+    }
+}
